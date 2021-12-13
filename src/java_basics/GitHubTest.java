784e05c893d838991317de00bc7e569052164d74
HaziqRidzwan <haziqridzwan@outlook.com>
2021-12-13 23:09:53 +0800

    Added step for push code to GitHub

--- src/java_basics/GitHubTest.java
+++ src/java_basics/GitHubTest.java
@@ -1,10 +1,15 @@
 package java_basics;
 
+
+// @author HaziqRidzwan
 public class GitHubTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// 1: Pull - tarik code orang lain yang kat server (cloud)
+		// 2: Commit (kalau ada changes) - commit code sendiri ke local server
+		// 3: Push - ke server
 	}
+	
 
 }
